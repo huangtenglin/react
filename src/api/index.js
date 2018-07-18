28fa4a9218f115d0377805ac4d882715506a7e1a
tenlin <1013573246@qq.com>
2018-07-18 23:30:20 +0800

    tijiao

--- src/api/index.js
+++ src/api/index.js
@@ -4,6 +4,6 @@ import ajax from './ajax';
 //暴露模块接口
 const BASE = '';
 
-export const reqRegister = (user) =>ajax.get(BASE + "/register",user,"POST");
+export const reqRegister = (user) =>ajax(BASE + "/register",user,"POST");
 
 export const reqLogin = (user)=>ajax(BASE +'/login',user,"POST");
