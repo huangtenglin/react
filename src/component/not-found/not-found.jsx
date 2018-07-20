7dffcb57d3ccd49725645ed5e9de6e25f9eb07a9
tenlin <1013573246@qq.com>
2018-07-20 09:55:45 +0800

    'sss'

---
+++ src/component/not-found/not-found.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class NoFound extends Component {
+
+  render () {
+    return (
+        <div>
+            <h1>很抱歉，没有找到</h1>
+        </div>
+    )
+  }
+
+}
+export default NoFound;
